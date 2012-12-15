57a05c253d8634eafc1d36444a7a156c5c5f1bd6
Drew DeVault <sir@cmpwn.com>
2012-12-15 16:01:25 -0700

    Added basic proxy structure

--- SMProxy/Program.cs
+++ SMProxy/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace SMProxy
@@ -10,6 +11,23 @@ namespace SMProxy
         static void Main(string[] args)
         {
             
+        }
+
+        private static void CreatePacketArray()
+        {
+            // For generating the PacketReader array dynamically.
+            string array = "";
+            var types = Assembly.GetExecutingAssembly().GetTypes().Where(
+                t => typeof(IPacket).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract);
+            for (int i = 0; i < 256; i++)
+            {
+                var type = types.FirstOrDefault(t => ((IPacket)Activator.CreateInstance(t)).Id == i);
+                if (type == null)
+                    array += "null,";
+                else
+                    array += "typeof(" + type.Name + "),";
+                array += " // 0x" + i.ToString("X2") + Environment.NewLine;
+            }
         }
     }
 }
--- SMProxy/Proxy.cs
+++ SMProxy/Proxy.cs
@@ -1,12 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace SMProxy
 {
     public class Proxy
     {
         public const int ProtocolVersion = 49;
+        public const int MillisecondsBetweenUpdates = 10;
+        
+        public NetworkStream Client { get; set; }
+        public NetworkStream Server { get; set; }
+        private MinecraftStream ClientStream { get; set; }
+        private MinecraftStream ServerStream { get; set; }
+        public Timer Timer { get; set; }
+
+        public Proxy(NetworkStream client, NetworkStream server)
+        {
+            Client = client;
+            Server = server;
+            ClientStream = new MinecraftStream(Client);
+            ServerStream = new MinecraftStream(Server);
+        }
+
+        public void Start()
+        {
+            Timer = new Timer(Tick, null, MillisecondsBetweenUpdates, Timeout.Infinite);
+        }
+
+        public void Stop()
+        {
+            Timer.Change(Timeout.Infinite, Timeout.Infinite);
+        }
+
+        private void Tick(object discarded)
+        {
+            // TODO: Fallback to raw proxy
+            UpdateServer();
+            UpdateClient();
+            Timer.Change(MillisecondsBetweenUpdates, Timeout.Infinite);
+        }
+
+        private void UpdateClient()
+        {
+            if (!Client.DataAvailable)
+                return;
+            var packet = PacketReader.ReadPacket(ClientStream);
+            packet.WritePacket(ServerStream);
+        }
+
+        private void UpdateServer()
+        {
+            if (!Server.DataAvailable)
+                return;
+            var packet = PacketReader.ReadPacket(ServerStream);
+            packet.WritePacket(ClientStream);
+        }
     }
 }
