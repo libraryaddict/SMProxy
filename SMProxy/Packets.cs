9afff27ff85406b8d7af9decdab7b03f2d2a7e17
Drew DeVault <sir@cmpwn.com>
2012-12-28 11:11:48 -0700

    Updated references to fNbt; closes #2

--- SMProxy/Packets.cs
+++ SMProxy/Packets.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using SMProxy.Metadata;
-using LibNbt;
+using fNbt;
 
 namespace SMProxy
 {
--- SMProxy/Slot.cs
+++ SMProxy/Slot.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using LibNbt;
+using fNbt;
 using System.IO;
 using System.IO.Compression;
 
